3765399b3521d22f8c354a341ff33040d4acff8d
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-11-15 17:20:45 -0300

    Refatorando a classe para criação de tags e adicionando o recurso para indentação das tags

--- TagFactory.py
+++ TagFactory.py
@@ -1,7 +1,51 @@
 class TagFactory:
 
-    def __init__(self,data):
+    def __init__(self,name,data=None,cdata=False):
+        self.__name = name
         self.__data = data
+        self.__cdata = cdata
+        self.__children = []
+        self.__indentation_space = 0
 
     def __str__(self):
-        return f"<{self.__data[1]}>![CDATA[{self.__data[0]}]]</{self.__data[1]}>"
+
+        msg = self.to_ident(self.__indentation_space) 
+
+        if self.__cdata:
+            msg += f"<{self.__name}>![CDATA[{self.__data}]]</{self.__name}>"
+            return msg
+
+
+        if len(self.__children) != 0:
+
+            msg += f"<{self.__name}>\n"
+            for child in self.__children:
+
+                msg += str(child) + "\n"
+
+            msg += self.to_ident(self.__indentation_space)
+            msg += f"</{self.__name}>"
+        else:
+            msg += f"<{self.__name}>{self.__data}</{self.__name}>"
+
+        return msg
+
+    def to_ident(self,tab_count):
+
+        msg = ""
+        for i in range(0, tab_count):
+            msg += "\t"
+
+        return msg
+        
+
+    def set_identation(self,value):
+        self.__indentation_space = value
+
+
+    def get_child(self,child_number):
+        return self.__children[child_number]
+
+    def append_child(self,child):
+        child.set_identation(self.__indentation_space + 1)
+        self.__children.append(child)
--- app.py
+++ app.py
@@ -4,29 +4,13 @@ from TagFactory import TagFactory
 
 tb = Tabela("planilha_base.xlsx","Página2")
 
-emprendimentos_list = []
+ads = TagFactory("ads")
+ads.append_child(TagFactory("ad"))
+ads.append_child(TagFactory("teste","teste"))
 
-#Construindo a lista de empreendimetos
-for i in range(2, tb.get_worksheet().max_row+1):
-    emprendimentos_list.append(Empreendimento(
-        tb.get_worksheet()[f"AK{i}"].value,
-        tb.get_worksheet()[f"G{i}"].value,
-        tb.get_worksheet()[f"K{i}"].value,
-        tb.get_worksheet()[f"B{i}"].value,
-        tb.get_worksheet()[f"A{i}"].value,
-        ))
+for city in tb.get_column("A",2):
+    ads.get_child(0).append_child(TagFactory("city",city.value,True))
 
-tags_list = []
-for empreendimento in emprendimentos_list:
-    tags_list.append(TagFactory(empreendimento.get_title()))
-    tags_list.append(TagFactory(empreendimento.get_email()))
-    tags_list.append(TagFactory(empreendimento.get_contact()))
-    tags_list.append(TagFactory(empreendimento.get_url()))
-    tags_list.append(TagFactory(empreendimento.get_price()))
-    tags_list.append(TagFactory(empreendimento.get_category()))
-    tags_list.append(TagFactory(empreendimento.get_city()))
-    tags_list.append(TagFactory(empreendimento.get_description()))
-    
+#ads.append_child(ad)
 
-for i in range(0, 8):
-    print(tags_list[i])
+print(ads)
